e6be455a313777405821da51df6dfd8f46d10c55
Craig Monger <craig.monger@nibrt.ie>
2017-01-19 12:54:21 +0000

    Update annotateTrinityResults.pl

--- annotateTrinityResults.pl
+++ annotateTrinityResults.pl
@@ -4,21 +4,21 @@
 
 #Usage:
 # annotateTrinityResults.pl <<$RENAMEDFASTA>> <<$BLASTRESULTS>>
-# e.g annotateTrinityResults.pl sirgTrinityBlx70lenE20.blout6.names.fa sirgTrinityBlx70lenE20.blout6  annotatedFastaFile.fa
+# e.g annotateTrinityResults.pl TrinityBlx70lenE20.blout6.names.fa TrinityBlx70lenE20.blout6  annotatedFastaFile.fa
 
 #Input files = the filtered by name output and the blastx with length results.
 
 #Input generation
 # Full length transcripts from blast
-	#blastx  -query sirgTrinity.fasta -db /mnt/work/database/uniprot_sprot.trinotate.pep -out blastFullLengthTranscripts.outfmt6 -evalue 1e-20 -num_threads 32 -max_target_seqs 1 -outfmt 6
+	#blastx  -query Trinity.fasta -db /mnt/work/database/uniprot_sprot.trinotate.pep -out blastFullLengthTranscripts.outfmt6 -evalue 1e-20 -num_threads 32 -max_target_seqs 1 -outfmt 6
 #Run the length/tophit script from trintiy
-	#/mnt/work/software/trinityrnaseq-Trinity-v2.3.2/util/analyze_blastPlus_topHit_coverage.pl blastFullLengthTranscripts.outfmt6 sirgTrinity.fasta /mnt/work/database/uniprot_sprot.trinotate.pep
+	#/mnt/work/software/trinityrnaseq-Trinity-v2.3.2/util/analyze_blastPlus_topHit_coverage.pl blastFullLengthTranscripts.outfmt6 Trinity.fasta /mnt/work/database/uniprot_sprot.trinotate.pep
 #Extract transcripts with E20 and 70% length 
-	#awk '{if ($14 >= 70) print $0}' blastFullLengthTranscripts.outfmt6.w_pct_hit_length > sirgTrinityBlx70lenE20.blout6
+	#awk '{if ($14 >= 70) print $0}' blastFullLengthTranscripts.outfmt6.w_pct_hit_length > TrinityBlx70lenE20.blout6
 #Take the ids of significant hits
-	#sed '1d' sirgTrinityBlx70lenE20.blout6  |cut -f 1 >sirgTrinityBlx70lenE20.blout6.names
+	#sed '1d' TrinityBlx70lenE20.blout6  |cut -f 1 >TrinityBlx70lenE20.blout6.names
 #Filter fasta file based on the results of blast hit ids
-	#filterbyname.sh in=sirgTrinity.fasta names=sirgTrinityBlx70lenE20.blout6.names include=t out=sirgTrinityBlx70lenE20.blout6.names.fa
+	#filterbyname.sh in=Trinity.fasta names=TrinityBlx70lenE20.blout6.names include=t out=TrinityBlx70lenE20.blout6.names.fa
 
 
 
